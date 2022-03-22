4c090a089248bb7b30db55277b4260d3410f831a
Grace Acton <gacton@bates.edu>
2022-03-22 14:51:27 +0000

    Prepare dataframe for heat map

--- extra/HeatMap_Prep.R
+++ extra/HeatMap_Prep.R
@@ -1,4 +1,16 @@
 
 # Heatmap Prep ------------------------------------------------------------
 
+# fix filter stuff: 
+categories <- board_games %>% 
+  select(category) %>% 
+  separate(category, into = c("category1", "category2", "category3", "category4"), sep = ",") %>% 
+  filter(category1 == "Card Game | Abstract Strategy | Wargame | Economic | Adventure | Bluffing | Action / Dexterity | Animals | Dice | Ancient")
 
+
+cat_counts <- categories %>% 
+  filter(is.na(category1) == FALSE,
+         is.na(category2) == FALSE) %>% 
+  group_by(category1, category2) %>% 
+  count() %>% 
+  arrange(desc(n))
